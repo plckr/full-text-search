234a391ed0c863d88cd1fa4135857eae002f82c4
Ricardo Reis <ricardo@significa.co>
2024-03-13 13:42:57 +0000

    refactor: raw sql

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,50 +1,54 @@
 import { PrismaClient } from '@prisma/client';
-import fs from 'node:fs';
 import * as csv from 'csv-parse';
+import fs from 'node:fs';
 
 const prisma = new PrismaClient();
 const parser = csv.parse({
-	delimiter: ','
+  delimiter: ','
 });
 
 async function seed() {
-	fs.createReadStream('./2024-02-21_dev_meeting_zara_products.csv').pipe(parser);
+  fs.createReadStream('./2024-02-21_dev_meeting_zara_products.csv').pipe(parser);
 
-	// Delete all records
-	await prisma.product.deleteMany({});
+  // Delete all records
+  await prisma.product.deleteMany({});
 
-	parser.on('readable', async () => {
-		let record;
-		let idx = 0;
-		while ((record = parser.read()) !== null) {
-			idx++;
-			if (idx === 1) continue;
+  parser.on('readable', async () => {
+    let record;
+    let idx = 0;
+    while ((record = parser.read()) !== null) {
+      idx++;
+      if (idx === 1) continue;
 
-			const [brand, url, sku, name, description, price, currency, scraped_at, terms, section] =
-				record;
-			await prisma.product.create({
-				data: {
-					brand,
-					url,
-					sku,
-					name,
-					description,
-					price: +price.replace(',', '.'),
-					currency,
-					scraped_at: new Date(scraped_at.split(',')[0]),
-					terms,
-					section
-				}
-			});
-		}
-	});
+      const [brand, url, sku, name, description, price, currency, scraped_at, terms, section] =
+        record;
+
+      // Should I pass items that don't have a name?
+      // if (!name) continue;
+
+      await prisma.product.create({
+        data: {
+          brand,
+          url,
+          sku,
+          name,
+          description,
+          price: +price.replace(',', '.'),
+          currency,
+          scraped_at: new Date(scraped_at.split(',')[0]),
+          terms,
+          section
+        }
+      });
+    }
+  });
 }
 
 seed()
-	.catch((e) => {
-		console.error(e);
-		process.exit(1);
-	})
-	.finally(() => {
-		return prisma.$disconnect;
-	});
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    return prisma.$disconnect;
+  });
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,41 @@
+import type { Product } from '@prisma/client';
 import { prisma } from '$lib/db.server';
 import { serialize } from '$lib/utils/prisma';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
-  const search = url.searchParams.get('search');
+  const search = url.searchParams.get('search') || '';
+  const page = Number(url.searchParams.get('page') || 1);
 
-  const products = await prisma.product.findMany({
-    skip: 0,
-    take: 18,
-    where: search ? { searchField: { search } } : undefined
-  });
+  const take = 18;
+  const skip = (page - 1) * take;
 
-  const count = await prisma.product.count();
+  const [products, [{ count }]] = await prisma.$transaction([
+    prisma.$queryRaw<Product[]>`
+      SELECT
+        "public"."products"."sku",
+        "public"."products"."brand",
+        "public"."products"."url",
+        "public"."products"."name",
+        "public"."products"."description",
+        "public"."products"."price",
+        "public"."products"."currency",
+        "public"."products"."scraped_at",
+        "public"."products"."terms",
+        "public"."products"."section",
+        "public"."products"."search_field"
+      FROM "public"."products"
+      WHERE 
+        ${search} = ''
+        OR to_tsvector(concat_ws(' ', "public"."products"."search_field")) @@ to_tsquery('english', ${search})
+      ORDER BY
+        ts_rank(to_tsvector(concat_ws(' ', "public"."products"."search_field")), to_tsquery('english', ${search})) DESC,
+        "public"."products"."sku" ASC
+      LIMIT ${take}
+      OFFSET ${skip}
+  `,
+    prisma.$queryRaw<[{ count: bigint }]>`SELECT COUNT(*) FROM "public"."products";`
+  ]);
 
-  return { products: serialize(products), count };
+  return { products: serialize(products), count: Number(count) };
 };
